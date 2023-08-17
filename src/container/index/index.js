4d7893746f63536e4c4d52993975099999c8e50e
Sensation02 <vasia1415@gmail.com>
2023-08-17 19:34:49 +0300

    master

---
+++ src/container/index/index.js
@@ -0,0 +1,5 @@
+import '../../script/test'
+import { Message } from '../../script/test'
+
+console.log('container')
+Message() // script
---
+++ src/script/test.js
@@ -0,0 +1,8 @@
+// console.log('script')
+
+const message = 'script'
+const Message = () => {
+  console.log('script')
+}
+export default message
+export { Message }
